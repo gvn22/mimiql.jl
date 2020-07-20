319c6a319a113c1fb949ea0c3f98d46be2bc5bf7
Girish Nivarti <nvgirishs@gmail.com>
2020-07-20 16:55:42 +0100

    Test for various parameters

--- mimicql.jl
+++ mimicql.jl
@@ -4,14 +4,14 @@ using Plots, PlotlyJS
 # lh -> 2D GQL analogue with one low & one high mode
 # lc -> 2D GCE2 analogue with one low mode & second cumulant
 
-lh = @ode_def begin
+lh = @ode_def_bare begin
 
     dx = α + β*(x*x + y*y) - x
     dy = β*(2.0*x*y) - y
 
 end β α
 
-lc = @ode_def begin
+lc = @ode_def_bare begin
 
     dx = α + β*(x*x + z) - x
     dz = 2.0*(-z + β*(2.0*x*z))
@@ -19,7 +19,10 @@ lc = @ode_def begin
 end β α
 
 # solve over a region in parameter space
-βs,αs   = [0.1,1.0,10.0],[0.01,0.1,1.0]
+# βs,αs   = [0.1,1.0,10.0],[0.01,0.1,1.0]
+βs = [10.0^i for i=-3:1:2]
+αs = [10.0^i for i=-3:1:2]
+
 px,py   = length(βs),length(αs)
 
 lnorms  = zeros(Float64,px,py)
@@ -36,13 +39,13 @@ for i ∈ CartesianIndices((1:px,1:py))
     println("IC: x = ",u0_lh[1], " y = ",u0_lh[2])
 
     tspan   = (0.0,100.0)
-    p       = [βs[i[1]],γs[i[2]]]
+    p       = [βs[i[1]],αs[i[2]]]
 
     prob_lh = ODEProblem(lh,u0_lh,tspan,p)
-    sol_lh  = solve(prob_lh,Tsit5(),adaptive=false,dt=0.005)
+    sol_lh  = solve(prob_lh,RK4(),adaptive=false,dt=0.001,abstol=1e-6,reltol=1e-6)
 
     prob_lc = ODEProblem(lc,u0_lc,tspan,p)
-    sol_lc  = solve(prob_lc,Tsit5(),adaptive=false,dt=0.005)
+    sol_lc  = solve(prob_lc,RK4(),adaptive=false,dt=0.001,abstol=1e-6,reltol=1e-6)
 
     l1 = [abs(x) for x in sol_lh[1,:]]
     l2 = [abs(x) for x in sol_lc[1,:]]
@@ -58,25 +61,26 @@ for i ∈ CartesianIndices((1:px,1:py))
 
     println("Diff: low = ", dl, " cumulant = ",dc)
 
-    g(x,y)  = (x,abs(y))
-    h(x,y)  = (x,abs(y^2))
+    # g(x,y)  = (x,abs(y))
+    # h(x,y)  = (x,abs(y^2))
 
-    plotly()
-
-    pl_lh   = Plots.plot(sol_lh,vars=(g,0,1),linewidth=1,xaxis="t",yaxis="abs(x)",title="low: GQL",legend=false)
-    pl_lc   = Plots.plot(sol_lc,vars=(g,0,1),linewidth=1,xaxis="t",yaxis="abs(x)",title="low: GCE2",legend=false)
-    pc_lh   = Plots.plot(sol_lh,vars=(h,0,2),linewidth=1,xaxis="t",yaxis="abs(y^2)",title="cumulant: GQL",legend=false)
-    pc_lc   = Plots.plot(sol_lc,vars=(g,0,2),linewidth=1,xaxis="t",yaxis="abs(z)",title="cumulant: GCE2",legend=false)
-
-    pcomp   = Plots.plot(Plots.plot(pl_lh,pl_lc,layout=(1,2)),Plots.plot(pc_lh,pc_lc,layout=(1,2)),layout=(2,1))
-
-    Plots.display(pcomp)
+    # plotly()
+    #
+    # pl_lh   = Plots.plot(sol_lh,vars=(g,0,1),linewidth=1,xaxis="t",yaxis="abs(x)",title="low: GQL",legend=false)
+    # pl_lc   = Plots.plot(sol_lc,vars=(g,0,1),linewidth=1,xaxis="t",yaxis="abs(x)",title="low: GCE2",legend=false)
+    # pc_lh   = Plots.plot(sol_lh,vars=(h,0,2),linewidth=1,xaxis="t",yaxis="abs(y^2)",title="cumulant: GQL",legend=false)
+    # pc_lc   = Plots.plot(sol_lc,vars=(g,0,2),linewidth=1,xaxis="t",yaxis="abs(z)",title="cumulant: GCE2",legend=false)
+    #
+    # pcomp   = Plots.plot(Plots.plot(pl_lh,pl_lc,layout=(1,2)),Plots.plot(pc_lh,pc_lc,layout=(1,2)),layout=(2,1))
+    #
+    # Plots.display(pcomp)
 
 end
 
-gr()
+# gr()
+pyplot()
 
 xs = [string(i) for i = βs]
 ys = [string(i) for i = αs]
 
-wireframe(xs,ys,lnorms',title=:"RK4")
+wireframe(xs,ys,lnorms',xaxis="β",yaxis="α",title=:"RK4")
