e495c8723378347782362e6cb5226481d85cca54
Girish Nivarti <nvgirishs@gmail.com>
2020-07-21 11:03:49 +0100

    Interchange parameters and iterate over non nans

--- mimiql.jl
+++ mimiql.jl
@@ -1,71 +1,67 @@
 using DifferentialEquations, LinearAlgebra, ParameterizedFunctions
 using Plots, PlotlyJS
+using Sundials
 
 # lh -> 2D GQL analogue with one low & one high mode
 # lc -> 2D GCE2 analogue with one low mode & second cumulant
 
-lh = @ode_def_bare begin
+lh = @ode_def_bare GQL begin
 
     dx = α + β*(x*x + y*y) - x
     dy = β*(2.0*x*y) - y
 
-end β α
+end α β
 
-lc = @ode_def_bare begin
+lc = @ode_def_bare GCE2 begin
 
     dx = α + β*(x*x + z) - x
     dz = 2.0*(-z + β*(2.0*x*z))
 
-end β α
+end α β
 
-# solve over a region in parameter space
-# βs,αs   = [0.1,1.0,10.0],[0.01,0.1,1.0]
+# solve over region in parameter space
+αs = [10.0^i for i=-2:1:0]
 βs = [10.0^i for i=-2:1:1]
-αs = [10.0^i for i=-2:1:1]
+lα,lβ = length(αs),length(βs)
+ldiffs,cdiffs = zeros(Float64,lα,lβ),zeros(Float64,lα,lβ)
 
-px,py   = length(βs),length(αs)
+tspan = (0.0,100.0)
 
-lnorms  = zeros(Float64,px,py)
-cnorms  = zeros(Float64,px,py)
+println(" ") # flush
 
-S = 100
+for i ∈ CartesianIndices((1:lα,1:lβ))
 
-for i ∈ CartesianIndices((1:px,1:py))
+    println("Loading parameters: α = ",αs[i[1]], " β = ",βs[i[2]])
 
-    println(" ")
-    println("Params: β = ",βs[i[1]], " α = ",αs[i[2]])
+    S = 0
+    while S < 100
 
-    tspan   = (0.0,100.0)
-
-    for j=1:1:S
-
-        u0_lh   = randn(ComplexF64,2)
+        u0_lh   = rand(ComplexF64,2)
         u0_lc   = [u0_lh[1], u0_lh[2]^2]
 
         # println("IC: x = ",u0_lh[1], " y = ",u0_lh[2])
 
-        p       = [βs[i[1]],αs[i[2]]]
+        p       = [αs[i[1]],βs[i[2]]]
 
         prob_lh = ODEProblem(lh,u0_lh,tspan,p)
-        sol_lh  = solve(prob_lh,Tsit5(),adaptive=false,dt=0.001,abstol=1e-6,reltol=1e-6)
+        sol_lh  = solve(prob_lh,RK4(),dense=true,abstol=1e-8,reltol=1e-8)
 
         prob_lc = ODEProblem(lc,u0_lc,tspan,p)
-        sol_lc  = solve(prob_lc,Tsit5(),adaptive=false,dt=0.001,abstol=1e-6,reltol=1e-6)
+        sol_lc  = solve(prob_lc,RK4(),dense=true,abstol=1e-8,reltol=1e-8)
 
-        l1 = [abs(x) for x in sol_lh[1,:]]
-        l2 = [abs(x) for x in sol_lc[1,:]]
+        normdx = norm([abs(sol_lh(j)[1]) - abs(sol_lc(j)[1]) for j=0:1.0:tspan[2]])
+        normdz = norm([abs(sol_lh(j)[2]^2) - abs(sol_lc(j)[2]) for j=0:1.0:tspan[2]])
 
-        c1 = [abs(y^2) for y in sol_lh[2,:]]
-        c2 = [abs(z) for z in sol_lc[2,:]]
+        if(!isnan(normdx) && !isnan(normdz))
 
-        dl = norm(l1 - l2)
-        dc = norm(c1 - c2)
-        if(!isnan(dl) && !isnan(dc))
-            lnorms[i] += dl
-            cnorms[i] += dc
+            ldiffs[i] += normdx
+            cdiffs[i] += normdz
+
+            S += 1
+
         end
 
-        println("Diff: low = ", dl, " cumulant = ",dc)
+        # println("Diff: low = ", dl, " cumulant = ",dc)
 
         # g(x,y)  = (x,abs(y))
         # h(x,y)  = (x,abs(y^2))
@@ -83,16 +79,16 @@ for i ∈ CartesianIndices((1:px,1:py))
 
     end
 
-    lnorms[i] /= S
-    cnorms[i] /= S
+    ldiffs[i] /= S
+    cdiffs[i] /= S
 
 end
 
 pyplot()
 
-xs = [string(i) for i = βs]
 ys = [string(i) for i = αs]
+xs = [string(i) for i = βs]
 
-p1 = wireframe(xs,ys,lnorms',zaxis="x",xaxis="β",yaxis="α",title="Tsit5")
-p2 = wireframe(xs,ys,cnorms',zaxis="z",xaxis="β",yaxis="α")
-# Plots.plot(p1,p2,layout=(1,2))
+p1 = wireframe(xs,ys,lnorms,zaxis="x",xaxis="β",yaxis="α",title="RK4")
+p2 = wireframe(xs,ys,cnorms,zaxis="z",xaxis="β",yaxis="α")
+Plots.plot(p1,p2,layout=(1,2))
